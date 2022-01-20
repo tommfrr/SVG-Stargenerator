ed612de29e968c9330e097862f83ad56a8d3bd85
Tom Franke <tom.franke1@mailbox.tu-dresden.de>
2022-01-20 21:52:59 +0000

    Sterngenerator.py added

---
+++ Sterngenerator.py
@@ -0,0 +1,52 @@
+import math
+
+def draw_sterne(x, y, ecken):
+    ra = 100 #Radius des äußeren Kreises
+    ri = 30  #Radius des inneren Kreises
+    pi = 3.141
+    alpha = 2*pi/ecken/2 #Kleinster Winkel
+
+    a_x = 0         #aktuelles x
+    a_y = 0         #aktuelles y
+    a_alpha = alpha #aktuelles Winkel
+
+    #Erstellen des Pfades, wird im Laufe des Programms erweitert und am Schluss ausgegeben
+    Pfad = "<path d=\"M " + str(x) + " " + str(y - ra) + " " #Cursor an Startposition bewegt
+
+    #Pro Schleifendurchlauf wird String um 2 Punkte erweitert (innerer und äußerer Kreis)
+    for i in range(ecken):
+        
+        #Berechnen des nächsten Punktes (innerer Ring)
+        a_x = round(ri * math.sin(a_alpha) + x) 
+        a_y = round(-ri * math.cos(a_alpha) + y)
+
+        #Ergänzen des Pfades (Punkt am inneren Ring)
+        Pfad = Pfad + "L " + str(a_x) + " " + str(a_y) + " "
+
+        #Winkel eine Einheit weitersetzen
+        a_alpha = a_alpha + alpha 
+        
+        #Berechnen des nächsten Punktes (äußerer Ring)
+        a_x = round(ra * math.sin(a_alpha) + x)
+        a_y = round(-ra * math.cos(a_alpha) + y)
+
+        #Ergänzen des Pfades (Punkt am äußeren Ring)
+        Pfad = Pfad + "L " + str(a_x) + " " + str(a_y) + " "
+
+        #Winkel eine Einheit weitersetzen
+        a_alpha = a_alpha + alpha
+
+    #Abschließen der Pfadanweisung
+    Pfad = Pfad + "z\" />"
+
+
+    print(Pfad)
+
+
+draw_sterne(200, 200, 5)
+draw_sterne(400, 200, 6)
+draw_sterne(600, 200, 7)
+draw_sterne(800, 200, 15)
+
+
+
